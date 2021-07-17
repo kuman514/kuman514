5eeb167f2602accf89ca26ec3757716f318d242b
kumankoishi.py <7iggif@naver.com>
2021-07-17 10:49:55 +0900

    Modified elements' id for hash links

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  loadFeatures('mains', 'mains.csv');
-  loadFeatures('training', 'training.csv');
-  loadFeatures('at-least-once', 'at_least_one.csv');
+  loadFeatures('article-mains', 'mains.csv');
+  loadFeatures('article-training', 'training.csv');
+  loadFeatures('article-at-least-once', 'at_least_one.csv');
 
-  loadProjects('projects-in-operation', 'projects_in_operation.csv');
-  loadProjects('projects-closed', 'projects_closed.csv');
-  loadProjects('toy-projects', 'toy_projects.csv');
-  loadProjects('other-projects', 'other_projects.csv');
+  loadProjects('article-projects-in-operation', 'projects_in_operation.csv');
+  loadProjects('article-projects-closed', 'projects_closed.csv');
+  loadProjects('article-toy-projects', 'toy_projects.csv');
+  loadProjects('article-other-projects', 'other_projects.csv');
 
-  loadFeatures('contact-me', 'contact.csv');
+  loadFeatures('article-contact-me', 'contact.csv');
 });
