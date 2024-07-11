035cdf5d40ec0ff22c24438677594d944d18f78e
kumankoishi.py <7iggif@naver.com>
2024-07-11 16:02:34 +0900

    feat: apply title & emphasize to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Title from '^/shared/Title';
+import Emphasize from '^/shared/Emphasize';
+
 export default function IntroPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <code className="font-mono font-bold">app/page.tsx</code>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
+      <Title customClassName="font-mono text-4xl sm:text-6xl">
+        <Emphasize customClassName="px-4">kuman514</Emphasize>
+      </Title>
+      <Title customClassName="font-mono text-lg text-center sm:text-2xl">
+        Frontend Developer Portfolio
+      </Title>
     </main>
   );
 }
