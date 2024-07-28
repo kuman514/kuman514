122e1dd0feb52d870caf98195f30244d082664e6
kumankoishi.py <7iggif@naver.com>
2024-07-29 04:35:14 +0900

    docs: change into real project lists' thumbnails

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,15 +1,13 @@
 import ProjectListItem from '^/entities/project/list-item';
 import Title from '^/shared/title';
 
-/**
- * @todo
- * This is fake content. Change into real one.
- */
+import ViteMinesweeperPng from '^/pages/vite-minesweeper/assets/vite-minesweeper.png';
+import YSOShmupRecords2Png from '^/pages/yso-shmup-records/assets/yso-shmup-records-2.png';
+import ExpoLogoWordMark from '^/pages/project-list/assets/logo-wordmark.png';
 
 export default function ProjectListPage() {
   return (
     <main className="flex min-h-screen flex-col px-8 py-24 gap-4 sm:px-24">
-      <code className="font-mono font-bold">app/projects/page.tsx</code>
       <Title customClassName="text-4xl sm:text-5xl md:text-6xl">
         프로젝트 목록
       </Title>
@@ -18,8 +16,7 @@ export default function ProjectListPage() {
           projectItem={{
             projectId: 'PrivateTeamProject',
             title: '비공개 팀 프로젝트',
-            thumbnailUrl:
-              'https://pbs.twimg.com/profile_images/1175746616545857538/JuT5t8dm.jpg',
+            thumbnailUrl: ExpoLogoWordMark.src,
             description: 'iOS + Android를 타겟으로 한 모바일 앱.',
             techStackTags: ['React Native', 'Expo'],
           }}
@@ -28,8 +25,7 @@ export default function ProjectListPage() {
           projectItem={{
             projectId: 'YSOShmupRecords',
             title: 'YSOShmupRecords',
-            thumbnailUrl:
-              'https://pbs.twimg.com/profile_images/1175746616545857538/JuT5t8dm.jpg',
+            thumbnailUrl: YSOShmupRecords2Png.src,
             description:
               '본인 YSO(kuman514)의 슈팅게임 기록을 열람하고 공유할 수 있게 만들기 위한 웹 사이트.',
             techStackTags: [
@@ -52,8 +48,7 @@ export default function ProjectListPage() {
           projectItem={{
             projectId: 'ViteMinesweeper',
             title: 'Vite 지뢰찾기 (ViteMinesweeper)',
-            thumbnailUrl:
-              'https://pbs.twimg.com/profile_images/1175746616545857538/JuT5t8dm.jpg',
+            thumbnailUrl: ViteMinesweeperPng.src,
             description:
               'React와 너비 우선 탐색 알고리즘을 활용하여 만든 Microsoft Minesweeper 스타일 지뢰찾기.',
             techStackTags: [
