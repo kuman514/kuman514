d3c090b3221c371b7d1cab10c971b7a46c1d5eec
kuman514 <hadjadj0@gmail.com>
2024-07-29 11:57:17 +0900

    improvement: apply section & improved layouts to whoami page

--- app/whoami/page.tsx
+++ app/whoami/page.tsx
@@ -1,57 +1,60 @@
+import Section from '^/entities/section';
 import ExternalAnchor from '^/shared/external-anchor';
 import Paragraph from '^/shared/paragraph';
-import SecondaryTitle from '^/shared/secondary-title';
 import Tag from '^/shared/tag';
 import Title from '^/shared/title';
 
 export default function WhoAmIPage() {
   return (
-    <main className="flex min-h-screen flex-col px-8 py-24 gap-4 sm:px-24">
-      <Title customClassName="text-4xl sm:text-5xl md:text-6xl">kuman514</Title>
+    <main className="flex min-h-screen flex-col px-8 py-24 gap-16 sm:px-24">
+      <Title customClassName="text-4xl sm:text-5xl md:text-6xl">
+        kuman514는...
+      </Title>
 
-      <SecondaryTitle customClassName="mt-4 text-2xl sm:text-3xl md:text-4xl">
-        자기소개
-      </SecondaryTitle>
-      <Paragraph customClassName="first-letter:text-2xl first-letter:text-green-600 first-letter:font-bold">
-        프론트엔드 엔지니어로서 React와 TypeScript를 다뤄왔으며, 이를 기반으로
-        고객이 겪고 있는 각종 문제를 해결해왔습니다. 이에 그치지 않고, Vite나
-        Next.js, React Native같은 기술 스택도 직접 연마하면서 비공개 팀
-        프로젝트나 YSOShmupRecords같은 개인 프로젝트에도 적용해나가고 있습니다.
-        더 나아가, 만든 앱의 안정성과 사용성을 향상시키기 위해, 사용자의
-        피드백을 반영하며 유지보수를 진행해오고 있습니다.
-      </Paragraph>
-      <Paragraph>
-        이러한 경험을 바탕으로, 항상 도전하는 자세로 안정성과 사용성에 기여하는
-        개발자가 되겠습니다. 감사합니다.
-      </Paragraph>
+      <Section title="자기소개" subsectionClassName="flex flex-col gap-4">
+        <Paragraph customClassName="first-letter:text-2xl first-letter:text-green-600 first-letter:font-bold">
+          프론트엔드 엔지니어로서 React와 TypeScript를 다뤄왔으며, 이를 기반으로
+          고객이 겪고 있는 각종 문제를 해결해왔습니다. 이에 그치지 않고, Vite나
+          Next.js, React Native같은 기술 스택도 직접 연마하면서 비공개 팀
+          프로젝트나 YSOShmupRecords같은 개인 프로젝트에도 적용해나가고
+          있습니다. 더 나아가, 만든 앱의 안정성과 사용성을 향상시키기 위해,
+          사용자의 피드백을 반영하며 유지보수를 진행해오고 있습니다.
+        </Paragraph>
+        <Paragraph>
+          이러한 경험을 바탕으로, 항상 도전하는 자세로 안정성과 사용성에
+          기여하는 개발자가 되겠습니다. 감사합니다.
+        </Paragraph>
+      </Section>
 
-      <SecondaryTitle customClassName="mt-4 text-2xl sm:text-3xl md:text-4xl">
-        현재 하고 있는 활동
-      </SecondaryTitle>
-      <Paragraph customClassName="first-letter:text-2xl first-letter:text-green-600 first-letter:font-bold">
-        Github에서 1일 1커밋 활동을 진행하고 있습니다. 1일 1커밋이란, 배운
-        내용이나 프로젝트 진행 등의 유의미한 커밋을 하루에 최소 1회 하는 활동을
-        말합니다. 저는 이러한 활동을 통해, 저의 개발 역량을 매일 조금씩이라도
-        꾸준히 향상시킬 수 있었습니다.
-      </Paragraph>
-      <Paragraph customClassName="first-letter:text-2xl first-letter:text-green-600 first-letter:font-bold">
-        [{' '}
-        <ExternalAnchor href={'https://github.com/kuman514'}>
-          kuman514의 Github 바로가기
-        </ExternalAnchor>{' '}
-        ]
-      </Paragraph>
+      <Section
+        title="현재 하고 있는 활동"
+        subsectionClassName="flex flex-col gap-4"
+      >
+        <Paragraph customClassName="first-letter:text-2xl first-letter:text-green-600 first-letter:font-bold">
+          Github에서 1일 1커밋 활동을 진행하고 있습니다. 1일 1커밋이란, 배운
+          내용이나 프로젝트 진행 등의 유의미한 커밋을 하루에 최소 1회 하는
+          활동을 말합니다. 저는 이러한 활동을 통해, 저의 개발 역량을 매일
+          조금씩이라도 꾸준히 향상시킬 수 있었습니다.
+        </Paragraph>
+        <section>
+          [{' '}
+          <ExternalAnchor href={'https://github.com/kuman514'}>
+            kuman514의 Github 바로가기
+          </ExternalAnchor>{' '}
+          ]
+        </section>
+      </Section>
 
-      <SecondaryTitle customClassName="mt-4 text-2xl sm:text-3xl md:text-4xl">
-        주력 기술 스택
-      </SecondaryTitle>
-      <div className="flex flex-row flex-wrap gap-2">
+      <Section
+        title="주력 기술 스택"
+        subsectionClassName="flex flex-row flex-wrap gap-2"
+      >
         <Tag>React</Tag>
         <Tag>TypeScript</Tag>
         <Tag>JavaScript</Tag>
         <Tag>HTML</Tag>
         <Tag>CSS</Tag>
-      </div>
+      </Section>
     </main>
   );
 }
