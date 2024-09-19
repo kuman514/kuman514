b9c57672568797e912742546d7ff200d18fe3f31
kumankoishi.py <7iggif@naver.com>
2024-09-19 22:49:19 +0900

    docs: add objectives to yso-shmup-records page & vite-minesweeper page

--- src/pages/yso-shmup-records/index.tsx
+++ src/pages/yso-shmup-records/index.tsx
@@ -87,6 +87,20 @@ export default function YSOShmupRecordsPage() {
       </Section>
 
       <Section
+        title="이 프로젝트를 만든 이유"
+        subsectionClassName="flex flex-row flex-wrap gap-2"
+      >
+        <Paragraph>
+          슈팅게임 동호인들과 더욱 원활하게 교류하기 위해 제작되었습니다.
+          기존에는 SNS나 디스코드 등의 메신저로 교류하였으나, 이러한 교류 수단은
+          다른 종류의 글이나 메시지까지 난잡하게 섞여 있어 슈팅게임 기록을
+          제대로 찾아보기 어려운 문제를 안고 있었습니다. 이러한 문제를
+          해결하고자 제가 달성한 각종 기록을 더욱 쉽게 열람하고 공유할 수 있는
+          사이트를 직접 개발하였습니다.
+        </Paragraph>
+      </Section>
+
+      <Section
         title="사용한 기술 스택"
         subsectionClassName="flex flex-row flex-wrap gap-2"
       >
