fa990dc0f4ee68db5bcae5211388957704a6145f
kumankoishi.py <7iggif@naver.com>
2025-05-23 02:38:39 +0900

    draft: add text content for yso-arcade-records

--- src/pages/yso-arcade-records/index.tsx
+++ src/pages/yso-arcade-records/index.tsx
@@ -65,15 +65,23 @@ export default function YSOArcadeRecordsPage() {
         <Paragraph>
           슈팅게임 동호인들과 더욱 원활하게 교류하기 위해 제작되었습니다.
           기존에는 SNS나 디스코드 등의 메신저로 교류하였으나, 이러한 교류 수단은
-          다른 종류의 글이나 메시지까지 난잡하게 섞여 있어 슈팅게임 기록을
+          다른 종류의 글이나 메시지까지 난잡하게 섞여 있어 슈팅게임 관련 내용을
           제대로 찾아보기 어려운 문제를 안고 있었습니다. 이러한 문제를
-          해결하고자 제가 달성한 각종 기록을 더욱 쉽게 열람하고 공유할 수 있는
-          사이트를 직접 개발하였습니다.
+          해결하고자 제가 달성한 각종 기록, 제게 인상깊었던 게임과 각종 아케이드
+          관련 사진 모음집을 더욱 쉽게 열람하고 공유할 수 있는 사이트를 직접
+          개발하였습니다.
         </Paragraph>
-        {/**
-         * @todo
-         * Append improvements from YSOShmupRecords
-         */}
+        <Paragraph>
+          또한, 이 프로젝트는 YSOShmupRecords로부터 이어지며, 레포지토리가
+          분산되어 있던 프론트엔드(YSOShmupRecords),
+          백엔드(YSOShmupRecordsBackend), 백오피스(Writronics)를 하나의
+          레포지토리(YSOArcadeRecords)에 통합하기 위해 제작되었습니다. 이에
+          그치지 않고, 라우팅 설정에 추가적인 작업이 필요한
+          react-router-dom으로부터 탈피하고, 파일 경로만 정해줘도 라우팅 설정이
+          끝나는, 더 간단한 라우팅 방식, 그리고 서버 사이드 렌더링을 통해
+          페이지의 로딩 시간을 감소시킬 필요가 있었습니다. 이러한 목적에 가장
+          적절한 방법이 Next.js 15를 사용하는 것이었습니다.
+        </Paragraph>
       </Section>
 
       <Section
@@ -86,6 +94,7 @@ export default function YSOArcadeRecordsPage() {
         <Tag>Axios</Tag>
         <Tag>Vercel</Tag>
         <Tag>Supabase</Tag>
+        <Tag>Tailwind CSS</Tag>
       </Section>
 
       <Section
@@ -115,13 +124,22 @@ export default function YSOArcadeRecordsPage() {
       </Section>
 
       <Section
-        title="Supabase와 서버 액션을 사용"
+        title="Supabase와 Server Actions 사용"
         subsectionClassName="flex flex-col gap-4"
       >
-        {/**
-         * @todo
-         * Add what I did with Supabase and Server Actions.
-         */}
+        <div className="w-full flex flex-row flex-wrap justify-center items-center gap-4"></div>
+        <Paragraph>
+          기록과 리뷰 등의 데이터를 더욱 쉽게 관리하기 위해,{' '}
+          <ExternalAnchor href="https://supabase.com/pricing">
+            무료 티어로도 API 무제한 호출이 가능한 Supabase
+          </ExternalAnchor>
+          와 Server Actions을 이용해 데이터베이스와 퍼블릭 버킷을 연동하여,
+          하나의 프로젝트에서 기록/리뷰 열람/추가/편집/삭제는 물론 직접 이미지
+          업로드하는 것까지 모두 가능하게 만들었습니다. 기록과 리뷰 열람은
+          누구나 할 수 있지만 추가/편집/삭제는 관리자 로그인이 필요하도록
+          만들었습니다. 다만, 이미지는 서버 액션에 올리기에 용량이 너무 큰 탓에,
+          이미지 업로드는 API Routes를 통해 진행하였습니다.
+        </Paragraph>
       </Section>
     </ProjectPageTemplate>
   );
