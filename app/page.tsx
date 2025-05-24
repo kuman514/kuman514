6f45d03e987d33db9e380fd69b3d6e3c24839e35
kuman514 <hadjadj0@gmail.com>
2025-05-25 02:06:36 +0900

    fix: use pure pixi-js for displaying even after routing back to intro via next links

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Emphasize from '^/shared/emphasize';
 import PixiJsApp from '^/shared/pixi-js';
-import BunnySprite from '^/shared/pixi-js/bunny-sprite';
 import Title from '^/shared/title';
 
 /**
@@ -11,9 +10,7 @@ import Title from '^/shared/title';
 export default function IntroPage() {
   return (
     <>
-      <PixiJsApp>
-        <BunnySprite />
-      </PixiJsApp>
+      <PixiJsApp />
       <main className="fixed x-0 y-0 flex min-h-screen flex-col items-center justify-center p-24 gap-4">
         <Title customClassName="font-mono text-4xl sm:text-6xl">
           <Emphasize customClassName="px-4">kuman514</Emphasize>
--- src/shared/pixi-js/index.tsx
+++ src/shared/pixi-js/index.tsx
@@ -1,28 +1,46 @@
 'use client';
 
-import { Application, extend } from '@pixi/react';
-import { Container, Graphics, Sprite } from 'pixi.js';
-import { ReactNode, useRef } from 'react';
+import { usePathname } from 'next/navigation';
+import { Application, Assets, Sprite } from 'pixi.js';
+import { useEffect, useRef } from 'react';
 
-// extend tells @pixi/react what Pixi.js components are available
-extend({
-  Container,
-  Graphics,
-  Sprite,
-});
-
-interface Props {
-  children?: ReactNode;
-}
-
-export default function PixiJsApp({ children }: Props) {
+export default function PixiJsApp() {
   const parentRef = useRef<HTMLDivElement>(null);
+  const pathName = usePathname();
 
-  // We'll wrap our components with an <Application> component to provide
-  // the Pixi.js Application context
+  useEffect(() => {
+    if (pathName !== '/' || !parentRef.current) {
+      return;
+    }
+
+    const pixiJsApp = new Application();
+    (async () => {
+      if (!parentRef.current) {
+        return;
+      }
+
+      await pixiJsApp.init({
+        backgroundAlpha: 0,
+        resizeTo: parentRef.current,
+      });
+      parentRef.current.appendChild(pixiJsApp.canvas);
+
+      await Assets.load({
+        alias: 'bunny-sprite',
+        src: 'https://pixijs.com/assets/bunny.png',
+      });
+      const sprite = Sprite.from('bunny-sprite');
+      sprite.anchor.set(0.5);
+      sprite.x = 100;
+      sprite.y = 100;
+      pixiJsApp.stage.addChild(sprite);
+    })();
+  }, [pathName]);
+
   return (
-    <div ref={parentRef} className="fixed x-0 y-0 w-full h-full opacity-25">
-      <Application resizeTo={parentRef}>{children}</Application>
-    </div>
+    <div
+      ref={parentRef}
+      className="fixed x-0 y-0 w-full h-full opacity-50"
+    ></div>
   );
 }
