c9ac9290a304085551ba4280e6b957b954066e50
kuman514 <hadjadj0@gmail.com>
2025-06-09 16:06:06 +0900

    fix: restore underline emphasizing background

--- src/shared/emphasize/index.tsx
+++ src/shared/emphasize/index.tsx
@@ -6,13 +6,9 @@ interface Props {
 }
 
 export default function Emphasize({ customClassName, children }: Props) {
-  /**
-   * @todo
-   * Restore underline backgrounds
-   */
   return (
     <strong
-      className={`bg-emphasize-underline from-transparent from-70% to-green-500 dark:to-green-600 to-70% ${customClassName}`}
+      className={`bg-linear-180 from-transparent from-70% to-green-500 dark:to-green-600 to-70% ${customClassName}`}
     >
       {children}
     </strong>
