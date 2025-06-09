368656bcabac7d8760e5fc87db3addb29402b261
kuman514 <hadjadj0@gmail.com>
2025-06-09 14:18:39 +0900

    chore: upgrade tailwind & auto-apply to modules

--- src/shared/tag/index.tsx
+++ src/shared/tag/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 export default function Tag({ customClassName, children }: Props) {
   return (
     <span
-      className={`font-mono bg-gray-300 text-red-700 text-sm px-1 py-0.5 rounded ${customClassName}`}
+      className={`font-mono bg-gray-300 text-red-700 text-sm px-1 py-0.5 rounded-sm ${customClassName}`}
     >
       {children}
     </span>
