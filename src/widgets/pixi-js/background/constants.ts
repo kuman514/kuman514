e118e467c0076843905c9386f04016908e87eeff
kuman514 <hadjadj0@gmail.com>
2025-08-04 10:57:16 +0900

    docs: define types for icon-comet background

---
+++ src/widgets/pixi-js/background/constants.ts
@@ -0,0 +1,2 @@
+export const SIN_60_DEG = Math.sin(Math.PI / 3);
+export const COS_60_DEG = Math.cos(Math.PI / 3);
---
+++ src/widgets/pixi-js/background/types.ts
@@ -0,0 +1,7 @@
+import { Sprite } from 'pixi.js';
+
+export interface IconComet {
+  sprite: Sprite;
+  scale: number;
+  speed: number;
+}
